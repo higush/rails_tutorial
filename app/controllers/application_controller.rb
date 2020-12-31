7fd90c88b02be9316ec6600ad6f6867015a9e86c
shigu-opsin <shingo.higuchi@ops-in.com>
2020-12-31 13:01:18 +0900

    fisrt commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  def hello
-    render html: "¡Hola, mundo!!"
-  end
-
-  def goodbye
-    randam_string = ('a'..'z').to_a.shuffle[0..7].join
-    render html: randam_string
-  end
 end
